df0d8270118889f76fc9fbc0a3452761f941dd6b
Tom Bakker <tom.bakker@truetrack.nl>
2019-03-19 15:00:18 +0100

    Send messages via rabbitMQ

--- routes/index.js
+++ routes/index.js
@@ -1,41 +1,51 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var uuid = require('uuid/v4');
+var uuid = require("uuid/v4");
+var bus = require("servicebus").bus();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
-router.get('/cars', function(req, res, next) {
+router.get("/cars", function(req, res, next) {
   Car.findAll().then(cars => {
     res.send(JSON.stringify(cars));
   });
 });
 
-router.post('/cars', function(req, res, next) {
+router.post("/cars", function(req, res, next) {
   const { io } = req.app;
-  const id = uuid();
 
-  io.emit('car created', { id: id });
+  console.log("CONNECTION WITH RABBITMQ READY");
+  const id = uuid();
+  io.emit("car created", { id: id });
   updateCar(id, io);
 
-  res.send('Started!');
+  res.send("Started!");
 });
 
 function updateCar(id, io) {
-  var _pathData = require('../bin/CarRoutes/Route' + getRandomInt(21));
+  var _pathData = require("../bin/CarRoutes/Route" + getRandomInt(21));
 
   var interval = setInterval(function() {
     var location = _pathData.pop();
 
-    if(!location) {
+    if (!location) {
       clearInterval(interval);
-      io.emit('car finished', { id });
+      io.emit("car finished", { id });
       return;
     }
 
-    io.emit('car update', { id: id, ...location });
+    bus.send("TrackingQueue", {
+      trackerID: id,
+      tripID: 12,
+      longitude: location.longitude,
+      latitude: location.latitude,
+      trackedAt: new Date()
+    });
+
+    io.emit("car update", { id: id, ...location });
   }, 1000);
 }
 
