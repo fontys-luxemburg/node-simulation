b883f978c5c7a51e687fdbbb4dd2b032c187ddc5
OlafvanDijk <o.olaf@hotmail.com>
2019-03-22 14:37:21 +0100

    issue 13 en 12
    - 1/2 kans dat json achterstevoren uitgelezen wordt
    - input veld om een aantal auto's in te laten spawnen
    - BUG als 1 auto klaar is met zijn rit zullen alle andere auto's die dezelfde rit volgde stoppen met rijden.

--- routes/index.js
+++ routes/index.js
@@ -8,16 +8,10 @@ router.get("/", function(req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-router.get("/cars", function(req, res, next) {
-  Car.findAll().then(cars => {
-    res.send(JSON.stringify(cars));
-  });
-});
-
 router.post("/cars", function(req, res, next) {
   const { io } = req.app;
-
   const id = uuid();
+
   io.emit("car created", { id: id });
   simulateCar(id, io);
 
@@ -25,7 +19,21 @@ router.post("/cars", function(req, res, next) {
 });
 
 function simulateCar(id, io) {
-  var _pathData = require("../bin/CarRoutes/Route" + getRandomInt(21));
+  //var _pathData = require("../bin/CarRoutes/Route" + getRandomInt(19));
+  
+  var randomint = getRandomInt(2);
+
+  const fs = require('fs');
+  const path = require("path");
+  let rawdata = fs.readFileSync(path.resolve( process.cwd(), './bin/CarRoutes/Route' + getRandomInt(19) + '.json'));
+  let _pathData = JSON.parse(rawdata);
+  console.log(randomint);
+  if(randomint == 1)
+  {
+    _pathData = _pathData.reverse();
+    console.log("Reverse!");
+  }
+
   var simulator = require("../models/geolocation-simulator")({
     coords: _pathData,
     speed: Math.floor(Math.random() * (120 - 50 + 1)) + 50
@@ -39,6 +47,7 @@ function simulateCar(id, io) {
     maximumAge: 0
   };
 
+  var doIt = false;
   var interval = setInterval(function() {
     simulator.getCurrentPosition(
       function(data) {
@@ -56,6 +65,7 @@ function simulateCar(id, io) {
       },
       function() {
         io.emit("car finished", { id });
+        doIt = true;
         clearInterval(interval);
         return;
       },
@@ -65,7 +75,7 @@ function simulateCar(id, io) {
 }
 
 function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+  return Math.floor(Math.random() * Math.floor(max) + 1);
 }
 
 module.exports = router;
