6775dcbc4faa41ab3639d89749fcbe6fff8fc5a0
Arjan <arjanlombarts@gmail.com>
2019-06-14 10:52:08 +0200

    fix foreach

--- routes/index.js
+++ routes/index.js
@@ -22,17 +22,21 @@ router.get("/", function(req, res, next) {
   //Spawn cars
   await getUUID();
 
-trackers.forEach(tracker => {
-  var id = tracker.trackerId;
+  for(var tracker of trackers){
+    console.log('tracker id = ' + tracker.trackerId);
+    
+    var id = tracker.trackerId;
 
-  getTripID();
-  var tripID = trip;
-  console.log('Trip nr: ' + trip);
+    await getTripID();
+    var tripID = trip;
+    console.log('Trip nr: ' + trip);
 
-  io.emit("car created", {id: id});
-  simulateCar(id, tripID, io);  
-});
-
+    io.emit("car created", {id: id});
+    simulateCar(id, tripID, io);  
+  }
+// trackers.forEach(tracker => {
+  
+// });
   res.send("Started!");
 });
 
