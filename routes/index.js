a58f5beda1601d777f35b4981a963ae796f9a892
Arjan <arjanlombarts@gmail.com>
2019-06-18 14:27:45 +0200

    trackingqueue2 naar trackingqueue

--- routes/index.js
+++ routes/index.js
@@ -89,7 +89,7 @@ function simulateCar(id, tripID, io) {
 		 amqp.connect('amqp://youtrack.tjidde.nl', function(err, conn) {
        
 			conn.createConfirmChannel(function(err, ch) {
-			var q = 'TrackingQueue2';
+			var q = 'TrackingQueue';
 
       ch.assertQueue(q, {durable: true});
       ch.assertQueue(q, {autoDelete: false});
